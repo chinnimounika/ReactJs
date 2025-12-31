850e8bd8d3b31ccf776d88f34b31b193b09c9ded
Mounika Chinni <chinnimounikarao777@gmail.com>
2025-12-31 08:38:23 +0530

    Create react52.js

---
+++ react52.js
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component{
+  render(){
+    return(
+      <>
+        <h2>Hello World!</h2>
+        <p>Welcome to the Java</p>
+      </>    
+      );
+  }
+}
+export default App;
